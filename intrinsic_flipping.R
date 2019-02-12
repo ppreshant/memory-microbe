e97ec1b286eced3f0d681be5e5484be188ff8803
ppreshant <ppresh.ant@gmail.com>
2019-02-12 06:03:01 +0000

    making plot with base R

--- intrinsic_flipping.R
+++ intrinsic_flipping.R
@@ -12,22 +12,32 @@ i0 <- 1e3 # number of integrase proteins
 kcat <- 1 # sec-1
 km <- 1e5 # copies
 
+# other initializations
+t <- seq(0,.05,.0001) # time sequence
+
 # calling libraries
 library(deSolve)
-
-# other initializations
-t <- seq(0,10,.01)
+# library(magrittr)
+# library(ggplot2)
 
 # differential equations
-func1 <- function(t,y, params)
+func_i <- function(t,y, params) # differential equation for intrinsic flipping - integrase independant
 {with(as.list(c(y,params)),
       {f <- y[1]; r <- y[2]
       df <- k1*r - k2*f
       dr <- -df
       dy <- list(c(df,dr))}
 )}
-  
+
+func_int <- function(t,y, params) # integrase dependant flipping differential eqn
+{with(as.list(c(y,params)),
+      {f <- y[1]; r <- y[2]
+      df <- k1*r - k2*f
+      dr <- -df
+      dy <- list(c(df,dr))}
+)}
+
 # Solving ODE for intrinsic flipping
-out1 <- ode(c(f = f0, r = r0), times = t, func1, c(k1,k2))
-# View(out1)
-with(out1, plot(time,f))
+out_i <- ode(c(f = f0, r = r0), times = t, func_i, c(k1,k2))
+# View(out_i)
+plot(out_i[,1], out_i[,2])
